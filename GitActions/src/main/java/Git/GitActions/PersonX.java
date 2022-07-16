cc752d065ba64d9d7d017ef07ec42aa0a0098593
Sethu <sethuparvathysubhash@gmail.com>
2022-07-16 15:49:58 +0530

    X Ist commit

---
+++ GitActions/src/main/java/Git/GitActions/PersonX.java
@@ -0,0 +1,12 @@
+package Git.GitActions;
+
+public class PersonX {
+
+	public static void main(String[] args) {
+		
+		
+	System.out.println("Person X 1 code");
+
+	}
+
+}
---
+++ GitActions/src/test/java/Git/GitActions/AppTest.java
@@ -0,0 +1,20 @@
+package Git.GitActions;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
